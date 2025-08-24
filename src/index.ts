70084b7f7b03606fa5c1ab862b5d92e241a92549
Yash Wankhade <yashwankhade5@gmail.com>
2025-08-24 17:36:13 +0530

    implement send and burn token

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,151 @@
 require('dotenv').config();
 import express from 'express';
 import { burnTokens, mintTokens, sendNativeTokens } from './mintTokens';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 const app = express();
 
-const Httpresponse = 
-{  "nativeTransfers": [ { "amount": 1000000000, "fromUserAccount": "Fq2j9VCHwbWZ286SixHsx2V4sTLur4iGteJsQQR9Rnzo", "toUserAccount": "B4ouTYDDw9cvDwWC9Cgqa6Ah8cDhV3RAWgr73AamiHSk" } ],  }
-const VAULT =`B4ouTYDDw9cvDwWC9Cgqa6Ah8cDhV3RAWgr73AamiHSk`
+const Httpresponse = [
+  {
+    "accountData": [
+      {
+        "account": "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b",
+        "nativeBalanceChange": -80001,
+        "tokenBalanceChanges": []
+      },
+      {
+        "account": "9T4j1h9e9MiAABLARbEgrruote6g6Fd4EgobmJvsVE3p",
+        "nativeBalanceChange": 0,
+        "tokenBalanceChanges": [
+          {
+            "mint": "DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4",
+            "rawTokenAmount": {
+              "decimals": 9,
+              "tokenAmount": "-1000000000"
+            },
+            "tokenAccount": "9T4j1h9e9MiAABLARbEgrruote6g6Fd4EgobmJvsVE3p",
+            "userAccount": "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b"
+          }
+        ]
+      },
+      {
+        "account": "EPgrYddNpPw82CKfj3bT5yyHt14vEPVF6kwnqPTSm6av",
+        "nativeBalanceChange": 0,
+        "tokenBalanceChanges": [
+          {
+            "mint": "DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4",
+            "rawTokenAmount": {
+              "decimals": 9,
+              "tokenAmount": "1000000000"
+            },
+            "tokenAccount": "EPgrYddNpPw82CKfj3bT5yyHt14vEPVF6kwnqPTSm6av",
+            "userAccount": "HGEkZnaCAg6USfEjgZndEZRvCVCkaRR8cxwhkmucKqXN"
+          }
+        ]
+      },
+      {
+        "account": "ComputeBudget111111111111111111111111111111",
+        "nativeBalanceChange": 0,
+        "tokenBalanceChanges": []
+      },
+      {
+        "account": "DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4",
+        "nativeBalanceChange": 0,
+        "tokenBalanceChanges": []
+      },
+      {
+        "account": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+        "nativeBalanceChange": 0,
+        "tokenBalanceChanges": []
+      }
+    ],
+    "description": "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b transferred 1 DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4 to HGEkZnaCAg6USfEjgZndEZRvCVCkaRR8cxwhkmucKqXN.",
+    "events": {},
+    "fee": 80001,
+    "feePayer": "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b",
+    "instructions": [
+      {
+        "accounts": [],
+        "data": "3P9srQMF9wRR",
+        "innerInstructions": [],
+        "programId": "ComputeBudget111111111111111111111111111111"
+      },
+      {
+        "accounts": [],
+        "data": "FrYDfD",
+        "innerInstructions": [],
+        "programId": "ComputeBudget111111111111111111111111111111"
+      },
+      {
+        "accounts": [
+          "9T4j1h9e9MiAABLARbEgrruote6g6Fd4EgobmJvsVE3p",
+          "DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4",
+          "EPgrYddNpPw82CKfj3bT5yyHt14vEPVF6kwnqPTSm6av",
+          "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b",
+          "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b"
+        ],
+        "data": "g7Xr2JSzc4cmW",
+        "innerInstructions": [],
+        "programId": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+      }
+    ],
+    "nativeTransfers": [],
+    "signature": "5yipQtMwJnFu2mzJhVGQx7uGxz6Z6tL8UjqN9ymjqMDfKJfTzT7LqDefpAJoGsjar5fCLbT9uFZ1J9Y1Y3d5weMZ",
+    "slot": 403236351,
+    "source": "SOLANA_PROGRAM_LIBRARY",
+    "timestamp": 1756032572,
+    "tokenTransfers": [
+      {
+        "fromTokenAccount": "9T4j1h9e9MiAABLARbEgrruote6g6Fd4EgobmJvsVE3p",
+        "fromUserAccount": "GmqBh3hA6Nndg9egWCGv4YbgMFbwhrrUrwdMFwFQuE8b",
+        "mint": "DNsixxb5ERannuurLjKE69JEoFApsRbfTYfhEdsKQ6Q4",
+        "toTokenAccount": "EPgrYddNpPw82CKfj3bT5yyHt14vEPVF6kwnqPTSm6av",
+        "toUserAccount": "HGEkZnaCAg6USfEjgZndEZRvCVCkaRR8cxwhkmucKqXN",
+        "tokenAmount": 1,
+        "tokenStandard": "Fungible"
+      }
+    ],
+    "transactionError": null,
+    "type": "TRANSFER"
+  }
+]
+const VAULT =`HGEkZnaCAg6USfEjgZndEZRvCVCkaRR8cxwhkmucKqXN`
 
 app.post('/helius', async(req, res) => {
-    const incomingtx = Httpresponse.nativeTransfers.find(x=>x.toUserAccount===VAULT)
+    let incomingtx;
+    let type;
+    let amount;
+    let tokenaccount;
+    if (Httpresponse[0].nativeTransfers.length){
+    incomingtx = (Httpresponse[0].nativeTransfers as any[]).find(x=>x.toUserAccount===VAULT)
+    console.log(incomingtx)
+    type = "received_native_sol";
+    amount = incomingtx.amount
+    }
+    else if (Httpresponse[0].tokenTransfers.length){
+incomingtx=(Httpresponse[0].tokenTransfers as any[]).find(x => x.toUserAccount === VAULT)
+console.log(incomingtx)
+type = "doesnt matter anything";  // this is not required only for developers understanding "undefined" will aslo work
+amount =  (incomingtx.tokenAmount)*LAMPORTS_PER_SOL
+tokenaccount = incomingtx.toTokenAccount
+    }
     if (!incomingtx) {
         res.json({"processed":"pr"})
         return
     }
-    console.log("hit the route")
+
     const fromAddress = incomingtx.fromUserAccount;
     const toAddress = incomingtx.toUserAccount;
-    const amount = incomingtx.amount;
-    const type = "received_native_sol";
+    
+    
+    
 
     if (type === "received_native_sol") {
+        console.log("reached")
         await mintTokens(fromAddress, toAddress, amount);
     } else {
         // What could go wrong here?
-        await burnTokens(fromAddress, toAddress, amount);
+        await burnTokens(fromAddress, toAddress, amount,tokenaccount);
         await sendNativeTokens(fromAddress, toAddress, amount);
     }
 
--- src/mintTokens.ts
+++ src/mintTokens.ts
@@ -1,5 +1,5 @@
-import { mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddress, getAccount } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddress, getAccount,burn } from "@solana/spl-token";
+import { Connection, Keypair, PublicKey, SystemProgram, Transaction,sendAndConfirmTransaction } from "@solana/web3.js";
 import bs58 from "bs58";
 import { PRIVATE_KEY, TOKEN_MINT_ADDRESS } from "./address";
 
@@ -8,29 +8,52 @@ export const mintTokens = async (fromAddress: string, toAddress: string, amount:
     const Token_Mint_Address= new PublicKey(TOKEN_MINT_ADDRESS)
     const userAddress = new PublicKey(fromAddress)
     const bs58privatekey = bs58.decode(PRIVATE_KEY)
-    let ataAddress;
-    ataAddress = await getAssociatedTokenAddress(Token_Mint_Address, userAddress)
+    let ataAddress = await getAssociatedTokenAddress(Token_Mint_Address, userAddress)
     const keypair = Keypair.fromSecretKey(bs58privatekey)
     try {
-        const ata = await getAccount(connection, ataAddress)
+        const ata = await getAccount(connection, ataAddress)  // this checks if ATA exist
+
     } catch (err) {
         ataAddress = (await getOrCreateAssociatedTokenAccount(connection, keypair, Token_Mint_Address, userAddress)).address
         const rent = await connection.getMinimumBalanceForRentExemption(165);
         amount = amount - rent;
         
     }
+    console.log("before mint")
     const c = await mintTo(connection, keypair, Token_Mint_Address, ataAddress, keypair, amount)
+     console.log("after mint",c)
    
 }
 
 
 
-export const burnTokens = async (fromAddress: string, toAddress: string, amount: number) => {
-    console.log("Burning tokens");
+export const burnTokens = async (fromAddress: string, toAddress: string, amount: number,tokenAccount:string) => {
+    const connection = new Connection("https://api.devnet.solana.com");
+    const Token_Mint_Address= new PublicKey(TOKEN_MINT_ADDRESS)
+    const decoded = bs58.decode(PRIVATE_KEY)
+    const keypair = Keypair.fromSecretKey(decoded)
+ const userAddress = new PublicKey(fromAddress)
+ const companyaddress = new PublicKey(toAddress)
+ const tokenaccount = new PublicKey(tokenAccount)
+const c =  await burn(connection,keypair,tokenaccount,Token_Mint_Address,keypair,amount)
+
+    console.log("Burning tokens",c);
 }
 
 
-
 export const sendNativeTokens = async (fromAddress: string, toAddress: string, amount: number) => {
-    console.log("Sending native tokens");
+const connection = new Connection("https://api.devnet.solana.com");
+const companyaddress = new PublicKey(toAddress)
+const useraddress = new PublicKey(fromAddress)
+  const decoded = bs58.decode(PRIVATE_KEY)
+    const keypair = Keypair.fromSecretKey(decoded)
+const transaction = new Transaction()
+const systransfer =  SystemProgram.transfer({
+    fromPubkey:companyaddress,
+    toPubkey:useraddress,
+    lamports:amount
+})
+transaction.add(systransfer)
+const signature = await sendAndConfirmTransaction(connection, transaction,[keypair]);
+    console.log("Sending native tokens",signature);
 }
